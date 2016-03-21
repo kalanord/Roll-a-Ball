c82f99b61a54b5d4fc4f1e59fd71e0b76a165fff
Federico Costa <fede.costa@live.com>
2016-03-21 19:14:58 -0300

    Created Pick Up prefab, its material and its rotation behaviour. Placed the Pick Ups over the Play Area.

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+    Vector3 rotation = new Vector3(15, 30, 45);
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        //This rotates the cube.
+        transform.Rotate(rotation * Time.deltaTime);
+
+        //We multiply movements by deltaTime in order to make them smooth and framerate independent.
+        //This is not needed in fixedUpdate since the time between them are always the same.
+	}
+}
