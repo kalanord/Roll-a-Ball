f2178e49f130ecbdc98c6e01f774c3b0f4fcf2f7
Federico Costa <fede.costa@live.com>
2016-03-21 23:27:18 -0300

    Added PickUpController to control their behavior when colliding with the Player. Optimized physics calculations by making Pick Ups dynamic game objects.

---
+++ Assets/Scripts/PickUpController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpController : MonoBehaviour {
+
+    /* In the tutorial, the player has the code to disable things it pick ups.
+     * This is bad because:
+     * 1) We are giving control of an object's state to another object, 
+     * instead of letting every object control their own.
+     * 2) The Pick Ups can only collide with the Player, but the Player can 
+     * collide with everything else on the level.
+     *
+     * So, I created another this other script, apart from the Rotator, just to control
+     * the Pick Up state in the game. */
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
